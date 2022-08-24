ba6173ccbcdfca010469c7b1abc6c668bcf9461b
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-24 14:01:16 -0500

    feat: completed all of the classes

--- customer.py
+++ customer.py
@@ -1,3 +1,13 @@
+from shopping_cart import ShoppingCart
+
 class Customer:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.customer_name = name
+        self.customer_cart = ShoppingCart()
+
+    def add_new_product(self, new_product):
+        self.customer_cart.add_product_to_cart(new_product)
+
+    def list_products(self):
+        for product in self.customer_cart.products_in_cart:
+            print(product)
