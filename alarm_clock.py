0301ca6ccd121806dde1bad0a9a98bcc27430126
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-24 13:31:53 -0500

    feat: created an alarmclock object and implement some of the methods

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,7 +1,7 @@
 class AlarmClock:
     def __init__(self):
-        self.current_time = ''
-        self.alarm_time = ''
+        self.current_time = '12:00'
+        self.alarm_time = '12:00'
         self.turned_on = False
 
     def set_current_time(self,):
@@ -15,5 +15,7 @@ class AlarmClock:
     def toggle_on_off(self):
         if self.turned_on == True:
             self.turned_on = False
+            print('The alarm has been switched off')
         else:
             self.turned_on = True
+            print('The alarm has been switched on')
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from alarm_clock import AlarmClock
+
+evans_alarm_clock = AlarmClock()
+
+print(evans_alarm_clock.current_time)
+evans_alarm_clock.set_alarm()
+evans_alarm_clock.toggle_on_off()
+
+
