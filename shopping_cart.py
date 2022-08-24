7806f30e316af5a3ee520e483e1e9927972a2a28
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-24 13:48:52 -0500

    feat: files of all new classes with class properties and methods

---
+++ shopping_cart.py
@@ -0,0 +1,12 @@
+class ShoppingCart:
+    def __init__(self):
+       self.products_in_cart = []
+
+    def calculate_total(self):
+        total = 0
+        for product in self.products_in_cart:
+            total += product.price
+        return total
+
+    def add_product_to_cart(self, given_product):
+        self.products_in_cart.append(given_product)
