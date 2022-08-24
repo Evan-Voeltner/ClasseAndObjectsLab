29b6de23acb3e54810e1461af81dceabcb1552d0
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-24 15:56:03 -0500

    fix: corrected sending texts

--- cell_phone.py
+++ cell_phone.py
@@ -4,19 +4,23 @@ class CellPhone:
         self.phone_number = ''
         self.contacts = [{
             'name' : 'Dad',
-            'phone_number' : '414-555-2102'
+            'phone_number' : '414-555-2102',
+            'recieved_messages' : []
         },
         {
             'name' : 'Mom',
-            'phone_number' : '414-555-6541'
+            'phone_number' : '414-555-6541',
+            'recieved_messages' : []
         },
         {
             'name' : 'Nevin',
-            'phone_number' : '414-555-1686'
+            'phone_number' : '414-555-1686',
+            'recieved_messages' : []
         },
         {
             'name' : "Nevin's Dad",
-            'phone_number' : '414-555-6969'
+            'phone_number' : '414-555-6969',
+            'recieved_messages' : []
         }]
         self.messages = []
         self.vibrate_mode = False
@@ -34,6 +38,11 @@ class CellPhone:
             print('Vibrate mode is on')
 
     def send_text_message(self):
+        reciever = input('Who are you sending this text to?: ').capitalize()
         text_message = input('Write your message: ')
-        print(text_message)
+        for contact in self.contacts:
+            if contact.get('name') == reciever:
+                contact.update({'recieved_messages' : [text_message]})
+                break
+        print(f'Your text to {reciever} says: {text_message}')
         return(text_message)
